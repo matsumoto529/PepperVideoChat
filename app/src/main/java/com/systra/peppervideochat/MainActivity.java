1fca18446b505de70c91bd4b99868bb7a0a31813
matsumoto529https://github.com/matsumoto529/PepperOperatorGit.git <matsumoto@systra.co.jp>
2019-11-07 16:41:10 +0900

    RobotLifecycleCallbacksを適用

--- app/src/main/java/com/systra/peppervideochat/MainActivity.java
+++ app/src/main/java/com/systra/peppervideochat/MainActivity.java
@@ -4,11 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+
+public class MainActivity extends AppCompatActivity implements RobotLifecycleCallbacks {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        QiSDK.register(this, this);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        QiSDK.unregister(this, this);
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+
     }
 }
